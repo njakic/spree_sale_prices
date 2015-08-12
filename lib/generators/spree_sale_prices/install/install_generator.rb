6e2cc830ebcb8b39217ce17d538bab705abbe953
Nikola Jakic <nikola@arsfutura.co>
2015-08-12 16:32:26 +0200

    Added functionality where you can set time also alongside date. Added generator data and dependecies for bootstrap-datetimepicker.

--- lib/generators/spree_sale_prices/install/install_generator.rb
+++ lib/generators/spree_sale_prices/install/install_generator.rb
@@ -2,6 +2,14 @@ module SpreeSalePrices
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "\n//= require moment \n//= require bootstrap-datetimepicker\n"
+      end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require bootstrap-datetimepicker\n", :before => /\*\//, :verbose => true
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_sale_prices'
       end
--- spree_sale_prices.gemspec
+++ spree_sale_prices.gemspec
@@ -25,6 +25,8 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'factory_girl', '~> 4.5'
   s.add_development_dependency 'simplecov', '~> 0.10'
   s.add_development_dependency 'sqlite3', '~> 1.3'
+  s.add_development_dependency 'momentjs-rails', '>= 2.9.0'
+  s.add_development_dependency 'bootstrap3-datetimepicker-rails', '~> 4.15.35'
   s.add_runtime_dependency 'slim-rails', '~> 3.0.1'
 
 end
